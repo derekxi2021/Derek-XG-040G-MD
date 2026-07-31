34fce1621b5fcb4f8082e441424adbd2bb9ed82c
derekxi2021 <88201015+derekxi2021@users.noreply.github.com>
2026-07-31 22:43:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,12 +20,3 @@ echo 'src-git helloworld https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2' >>feeds.conf.default
-# 2. 判断并进入 openwrt 目录（兼容 Actions 环境与本地环境）
-if [ -d "openwrt" ]; then
-    cd openwrt
-fi
-
-# 3. 在 openwrt/package/ 内部创建 custom 目录并 clone 仓库
-mkdir -p package/custom
-git clone --depth=1 https://github.com/sbwml/openwrt-vlmcsd.git package/custom/vlmcsd
-git clone --depth=1 https://github.com/sbwml/luci-app-vlmcsd.git package/custom/luci-app-vlmcsd
