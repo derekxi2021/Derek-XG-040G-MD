e8c52cd05157d119add62de4f9712e880e6d0748
derekxi2021 <88201015+derekxi2021@users.noreply.github.com>
2026-08-05 14:48:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,4 @@ echo 'src-git helloworld https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2' >>feeds.conf.default
+echo ‘src-git airohanpu https://github.com/rchen14b/luci-app-airoha-npu’ >>feeds.conf.default
